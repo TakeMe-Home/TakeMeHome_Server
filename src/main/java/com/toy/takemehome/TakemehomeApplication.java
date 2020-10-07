a1d1feb45ca5e5a2d3795ff4266ba235309704a5
BangKiHyun <rlrlvh@naver.com>
2020-10-07 16:44:10 +0900

    init

---
+++ src/main/java/com/toy/takemehome/TakemehomeApplication.java
@@ -0,0 +1,13 @@
+package com.toy.takemehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakemehomeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TakemehomeApplication.class, args);
+    }
+
+}
