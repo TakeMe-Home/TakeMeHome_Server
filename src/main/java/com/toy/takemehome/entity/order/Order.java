63774eafa412a6acbe088adb64726ecdeb9a3183
BangKiHyun <rlrlvh@naver.com>
2020-10-21 16:10:28 +0900

    Feat Order Entity

---
+++ src/main/java/com/toy/takemehome/entity/order/Order.java
@@ -0,0 +1,46 @@
+package com.toy.takemehome.entity.order;
+
+import com.toy.takemehome.entity.BaseTimeEntity;
+import com.toy.takemehome.entity.customer.Customer;
+import com.toy.takemehome.entity.delivery.Delivery;
+import com.toy.takemehome.entity.restaurant.Restaurant;
+import com.toy.takemehome.entity.rider.Rider;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+import static javax.persistence.GenerationType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Order extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "restaurant_id")
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "rider_id")
+    private Rider rider;
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+}
