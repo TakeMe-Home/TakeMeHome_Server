a501200df0ac363a97edc3968610f2e6fe16ca9c
BangKiHyun <rlrlvh@naver.com>
2020-11-10 23:32:14 +0900

    Feat calculateDistance
    좌표 기반 두 위치의 거리 알려주는 기능

--- src/main/java/com/toy/takemehome/entity/Location.java
+++ src/main/java/com/toy/takemehome/entity/Location.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
+import static java.lang.Math.*;
+
 @Embeddable
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -17,5 +19,35 @@ public class Location {
     public Location(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double calculateDistance(Location location) {
+        double theta = this.y - location.y;
+        double dist = (sin(convertDegToRad(this.x)) * sin(convertDegToRad(location.x)))
+                + (cos(convertDegToRad(this.x)) * cos(convertDegToRad(location.x))
+                * cos(convertDegToRad(theta)));
+
+        dist = acos(dist);
+        dist = convertRadToDeg(dist);
+        dist *= (60 * 1.1515);
+
+        final double kilometer = convertKilometer(dist);
+        return convertDecimalPointFirst(kilometer);
+    }
+
+    private double convertDegToRad(double deg) {
+        return (deg * PI / 180.0);
+    }
+
+    private double convertRadToDeg(double rad) {
+        return (rad * 180 / PI);
+    }
+
+    private double convertKilometer(double dist) {
+        return dist * 1.609344;
+    }
+
+    private double convertDecimalPointFirst(double kilometer) {
+        return ceil((kilometer * 10)) / 10.0;
     }
 }
