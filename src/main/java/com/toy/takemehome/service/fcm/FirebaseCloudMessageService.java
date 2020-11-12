aa548b447b320a21d229361ce3947c3db633f110
BangKiHyun <rlrlvh@naver.com>
2020-11-12 18:43:59 +0900

    change url

--- src/main/java/com/toy/takemehome/service/fcm/FirebaseCloudMessageService.java
+++ src/main/java/com/toy/takemehome/service/fcm/FirebaseCloudMessageService.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class FirebaseCloudMessageService {
     private static final String API_URL =
-            "https://fcm.googleapis.com/v1/projects/tourcash-13092/messages:send";
+            "https://fcm.googleapis.com/v1/projects/toy-takemehome/messages:send";
     private final ObjectMapper objectMapper;
 
     public void sendMessageTo(String targetToken, String title, String body) throws IOException {
