f7a082e7b0fe344ee8bdfad9876c94513935455b
BangKiHyun <rlrlvh@naver.com>
2020-11-19 17:47:54 +0900

    Fix validate check empty

--- src/main/java/com/toy/takemehome/dto/menu/MenuIdCounts.java
+++ src/main/java/com/toy/takemehome/dto/menu/MenuIdCounts.java
@@ -3,12 +3,14 @@ package com.toy.takemehome.dto.menu;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
 @Getter
 @NoArgsConstructor
 public class MenuIdCounts {
 
+    @NotEmpty
     private List<MenuIdCount> menuIdCounts;
 
     public MenuIdCounts(List<MenuIdCount> menuIdCounts) {
